aecd5bde8d6f7cb08116e2e45af5dd1c6dd22477
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-07-07 23:51:57 +0200

    modal with Lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,32 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const galleryList = galleryItems.map(({ preview, original, description }) =>
+    `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+).join('');
+
+gallery.insertAdjacentHTML("beforeend", galleryList);
+
+gallery.addEventListener('click', selectImg);
+
+function selectImg(evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+    basicLightbox.create(`
+		<img src="${evt.target.dataset.source}">
+	`).show();
+};
